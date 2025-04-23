40ad798cc45a6d8a99578c4137fd6033dbd4c269
Berkan Aydın <106911385+berkan2323@users.noreply.github.com>
2025-04-23 22:44:05 +0300

    Add files via upload

---
+++ WatchShop/WatchShop/WatchShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchShopApp.swift
+//  WatchShop
+//
+//  Created by Berkan Aydın on 23.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct WatchShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
